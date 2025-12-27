0bbe9683f0a061c101675c3980a441c3faed0449
Vladimir Simanovskii <v.simanovsky@gmail.com>
2025-12-28 02:03:41 +0300

    f

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,2 +1,2 @@
-export { signOut as logout, signInWithEmail, signUpWithEmail } from './api/auth'
+export { logout, signInWithEmail, signUpWithEmail } from './api/auth'
 export { default as AuthForm } from './ui/AuthForm.vue'
