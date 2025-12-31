dec337cbf3f1af6a3070b7c905b48d4352c6e3ac
Vladimir Simanovskii <v.simanovsky@gmail.com>
2025-12-31 19:48:12 +0300

    tooltip

--- src/app/providers/primevue/index.ts
+++ src/app/providers/primevue/index.ts
@@ -2,11 +2,13 @@ import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
 import ToastService from 'primevue/toastservice'
 import ConfirmationService from 'primevue/confirmationservice'
+import Tooltip from 'primevue/tooltip'
 import type { App } from 'vue'
 
 export const setupPrimeVue = (app: App) => {
   app.use(ToastService)
   app.use(ConfirmationService)
+  app.directive('tooltip', Tooltip)
   app.use(PrimeVue, {
     theme: {
       preset: Aura,
